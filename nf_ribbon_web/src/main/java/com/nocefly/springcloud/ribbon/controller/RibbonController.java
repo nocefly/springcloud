69b93e038a94d7ac3f6c45334fa044eea27151b1
wu <nocefly@163.com>
2019-06-21 00:40:43 +0800

    1、添加ribbon工程

--- nf_ribbon_web/src/main/java/com/nocefly/springcloud/ribbon/controller/RibbonController.java
+++ nf_ribbon_web/src/main/java/com/nocefly/springcloud/ribbon/controller/RibbonController.java
@@ -1,27 +1,23 @@
-package com.nocefly.springcloud.hello.controller;
+package com.nocefly.springcloud.ribbon.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class HelloController {
+public class RibbonController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    private DiscoveryClient client;
+    private RestTemplate restTemplate;
 
-    @Value("${server.port}")
-    private String port;
-
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @RequestMapping(value = "/ribbon_hello",method = RequestMethod.GET)
     public String hello(){
-        return String.format("%s port : hello world!",port);
+        return restTemplate.getForEntity("http://NF-HELLO-WEB/hello",String.class).getBody();
     }
 
 }
